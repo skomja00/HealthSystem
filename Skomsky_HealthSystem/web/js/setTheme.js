f06a95a3ffbc5eb304ac4373a5ce6a5ae17be722
James Skomsky <james.skomsky@gmail.com>
2020-01-15 01:59:36 -0500

    working on theme

--- Skomsky_HealthSystem/web/js/setTheme.js
+++ Skomsky_HealthSystem/web/js/setTheme.js
@@ -1,114 +1,129 @@
-/* https://color.adobe.com/ has a handy "Copy as XML" button. 
- * I pasted below, and used the xml string to start to build a 
- * set of theme colors. This hash
- * is extendible to as many color names as in the xml pallette
+/* 
+ * setTheme() will style/theme a set of properties for specifically named HTML 
+ * elements using a hash of named colors  from an xml pallette. color.adobe.com 
+ * provides the "copy to xml" function used in palleteDoc(). Found some info below 
+ * from w3schools.com regarding the DOM: 
+
  * 
- * 1. copy/paste "Copy as XML" see https://color.adobe.com/
- * 2. then add " (quotes) to make it a javascript string 
+ *     "The DOM defines a standard for accessing and manipulating HTML and XML documents.
+ *     It presents an XML document as a tree-structure. The XML DOM is a standard for 
+ *     how to get, change, add, or delete XML elements. Understanding the DOM 
+ *     is a must for anyone working with HTML or XML."
+ * 
+ * TODO: Would be nice to make the XML a settings accessible to 
+ * the user to customize in an external xml file or anywhere other than 
+ * hard-coded in the palletteDoc() fn. 
  * 
  */
 
 "use strict";
 
-function setTheme () {
+function setTheme (palId) {
 
-    var pal = getPallette();
+//    document.cookie = "darkLightThemeToggle=1";
+//    var toogle = document.cookie;
+    var hash = getPallette(palId);
+    document.getElementById("content").style.backgroundColor = hash["darkTrans"];
+    document.getElementById("svcs").style.backgroundColor = hash["lightest"];
+    document.getElementById("busofc").style.backgroundColor = hash["lightest"];
+    document.getElementById("docs").style.backgroundColor = hash["lightest"];
+    document.getElementById("footer").style.backgroundColor = hash["lighter"];
 
-    /* general theme styling */
-    document.getElementById("nav").style.backgroundColor = pal["dark"];
-    document.getElementById("content").style.backgroundColor = pal["darkTrans"];
-    document.getElementById("svcs").style.backgroundColor = pal["lightest"];
-    document.getElementById("busOfc").style.backgroundColor = pal["lightest"];
-    document.getElementById("docs").style.backgroundColor = pal["lightest"];
-    document.getElementById("footer").style.backgroundColor = pal["lighter"];
-
-    /* title theme styling */
-    document.getElementById("title").style.backgroundColor = pal["titleBackgroundColor"];
-
-    /* boxShadow:  
-    h-offset: positive shadow to the right, negative to the left, 0 around left/right perimeter 
-    v-offset: positive shadow below, negative above, 0 around top/bottom perimeter 
-    blur: the higher the blur radius, the more blurred the shadow will be
-    spread: spread radius. A positive increases the size of the shadow, a negative value decreases the size of the shadow*/
-    document.getElementById("title").style.boxShadow = "0px 0px 9px 7px " + pal["titleBoxShadow"];
-    document.getElementById("title").style.color = pal["titleColor"];
-    document.getElementById("title").style.textShadow = "0px 0px 10px " + pal["titleTextShadow"];
+    /*  styling */
+    document.getElementById("title").style.backgroundColor = hash["titleBackgroundColor"];
+    document.getElementById("title").style.boxShadow = "0px 0px 9px 7px " + hash["titleBoxShadow"];
+    document.getElementById("title").style.color = hash["titleColor"];
+    document.getElementById("title").style.textShadow = "0px 0px 10px " + hash["titleTextShadow"];
+    document.getElementById("nav").style.border = "2px solid " + hash["navBorder"];
+    document.getElementById("nav").style.backgroundColor = hash["navBackground"];
+    /*document.getElementById("svcs").style.border = "2px solid " + hash["listItemHover"];
+    document.getElementById("busofc").style.border = "2px solid " + hash["listItemHover"];
+    document.getElementById("docs").style.border = "2px solid " + hash["listItemHover"];*/
     
-    function getPallette() {
-        var i, r, g, b, a, rgba;
-        var pal = {}; /* hash: key = descriptive color name */ 
-                      /*       value = "RGBA(r,g,b,a)" string   */
-        var xml = pallette();
-        var child_nodes = xmlDoc.documentElement.childNodes;  
-        child_nodes.forEach() {
-            var x = xml.getElementsByTagName("color"); /* for each color */
-            for (i = 0; i <x.length; i++) {            /* get each r, g, b, a value */ 
-                r = x[i].getAttribute("r");              
-                g = x[i].getAttribute("g");
-                b = x[i].getAttribute("b");
-                a = x[i].getAttribute("a");
-                if (a === null) /* color.adobe.com doesn't give alpha values */
-                {               /* if not added alpha will defualt to l */
-                    a = "1";
-                }
-                rgba = "rgba(" + r + ", " + 
-                                 g + ", " +
-                                 b + ", " +
-                                 a + ")";
-              pal[x[i].getAttribute("name")] = rgba; /* "name" will be hash key */
+    function getPallette(palId) {
+        var rgba;
+        var alpha;
+        var child;
+        var hash = {}; /* hash: key = descriptive color name */ 
+                       /*       value = "RGBA(r,g,b,a)" string   */
+        var xmlDoc = palletteDoc();
+        var pallette = xmlDoc.getElementById(palId); 
+        child = pallette.firstChild;                           /* this is the first rgba color */
+        for (var i = 0; i < pallette.childNodes.length; i++) { /* for each get the rgba attrs */     
+            rgba = "rgba(";
+            rgba += child.getAttribute("r") + ", ";                
+            rgba += child.getAttribute("g") + ", ";     
+            rgba += child.getAttribute("b") + ", ";
+            alpha = child.getAttribute("a");
+            if (alpha === null) /* if not given alpha will defualt to l */
+            { 
+                rgba += "1)";
+            } 
+            else
+            {
+                rgba += alpha + ")";                
             }
+            hash[child.getAttribute("name")] = rgba; /* "name" will be hash key */
+            child = child.nextSibling;
+            rgba = "";           
         }
-        return pal;
+        return hash;
     }
-    function pallette() {
+    function palletteDoc() {
         var colorPalletteStr = 
-            /* TODO: Would be nice to make the XML a settings accessible to 
-            * the user instead of the hard-coded in the pallette() fn. */
-            "<Spectrum>" +
-                "<LightPallette>" +
-                /* general theme colors */
-                    "<color name='darker' rgb='A7DDF2' r='167' g='221' b='242' />" +
-                    "<color name='dark' rgb='A7E4F2' r='167' g='228' b='242' />" +
+            "<Root>" +
+                "<Pallette id='LightPallette'>" +
+                    "<color name='darker'    rgb='A7DDF2' r='167' g='221' b='242' />" +
+                    "<color name='dark'      rgb='A7E4F2' r='167' g='228' b='242' />" +
                     "<color name='darkTrans' rgb='A7E4F2' r='167' g='228' b='242' a='.5' />" +
-                    "<color name='light' rgb='DCEEF2' r='220' g='238' b='242' />" +
-                    "<color name='lighter' rgb='B6F2F2' r='182' g='242' b='242' />" +
-                    "<color name='lightest' rgb='F2F2DF' r='242' g='242' b='223' />" +
-                    /* light title theme colors */
-                    "<color name='titleBackgroundColor' rgb='A7E4F2' r='167' g='228' b='242' />" +
-                    "<color name='titleBoxShadow' rgb='CCCCCC' r='204' g='204' b='204' />" +
-                    "<color name='titleColor' rgb='FFFFFF' r='255' g='255' b='255' />" +
-                    "<color name='titleTextShadow' rgb='000000' r='000' g='000' b='000' />" +
-                "</LightPallette>" +
-                "<DarkPallette>" +
-                    /* general theme colors */
-                    "<color name='darker' rgb='A7DDF2' r='167' g='221' b='242' />" +
-                    "<color name='dark' rgb='A7E4F2' r='167' g='228' b='242' />" +
-                    "<color name='darkTrans' rgb='A7E4F2' r='167' g='228' b='242' a='.5' />" +
-                    "<color name='light' rgb='DCEEF2' r='220' g='238' b='242' />" +
-                    "<color name='lighter' rgb='B6F2F2' r='182' g='242' b='242' />" +
-                    "<color name='lightest' rgb='F2F2DF' r='242' g='242' b='223' />" +
-                    /* dark title theme colors */
-                    "<color name='titleBackgroundColor' rgb='A7DDF2' r='167' g='221' b='242' />" +
-                    "<color name='titleBoxShadow' rgb='CCCCCC' r='204' g='204' b='204' />" +
-                    "<color name='titleColor' rgb='FFFFFF' r='255' g='255' b='255' />" +
-                    "<color name='titleTextShadow' rgb='000000' r='000' g='000' b='000' />" +
-                "</DarkPallette>" +
-            "</Spectrum>";
+                    "<color name='light'     rgb='DCEEF2' r='220' g='238' b='242' />" +
+                    "<color name='lighter'   rgb='B6F2F2' r='182' g='242' b='242' />" +
+                    "<color name='lightest'  rgb='F2F2DF' r='242' g='242' b='223' />" +
+                    "<color name='titleBackgroundColor' rgb='A7E4F2' r='167' g='228' b='242' a='.5' />" +
+                    "<color name='titleBoxShadow'       rgb='CCCCCC' r='204' g='204' b='204' />" +
+                    "<color name='titleColor'           rgb='FFFFFF' r='355' g='255' b='255' />" +
+                    "<color name='titleTextShadow'      rgb='000000' r='000' g='000' b='000' />" +
+                    "<color name='navBorder'            rgb='B6F2F2' r='182' g='242' b='242' />" +
+                    "<color name='navBackGround'        rgb='B6F2F2' r='182' g='242' b='242'  a='.5' />" +
+                    "<color name='listItemHover'        rgb='B6F2F2' r='182' g='242' b='242' />" +
+                "</Pallette>" +
+                "<Pallette id='DarkPallette'>" +
+                    "<color name='darker'    rgb='4F82B3' r='79' g='130' b='179' />" +
+                    "<color name='dark'      rgb='A3D3FF' r='163' g='211' b='255' />" +
+                    "<color name='darkTrans' rgb='A3D3FF' r='163' g='211' b='255' a='.5' />" +
+                    "<color name='light'     rgb='89C5FF' r='137' g='197' b='255' />" +
+                    "<color name='lighter'   rgb='B3863D' r='179' g='134' b='61' />" +
+                    "<color name='lightest'  rgb='FFD38A' r='255' g='211' b='138' />" +
+                    "<color name='titleBackgroundColor' rgb='4F82B3' r='079' g='130' b='179' />" +
+                    "<color name='titleBoxShadow'       rgb='FFD38A' r='255' g='211' b='138' />" +
+                    "<color name='titleColor'           rgb='A3D3FF' r='163' g='211' b='255' />" +
+                    "<color name='titleTextShadow'      rgb='B3863D' r='179' g='134' b='061' />" +
+                    "<color name='navBorder'            rgb='A3D3FF' r='163' g='211' b='255' />" +
+                    "<color name='navBackground'        rgb='A3D3FF' r='163' g='211' b='255' a='.5' />" +
+                    "<color name='listItemHover'        rgb='B6F2F2' r='182' g='242' b='242' />" +
+                "</Pallette>" +
+            "</Root>";
         var xmlDoc = document.implementation.createDocument("", "", null);
-        var parser = new DOMParser(); /* see https://www.w3schools.com/xml/xml_parser.asp */
+        var parser = new DOMParser();
         xmlDoc = parser.parseFromString(colorPalletteStr,"text/xml");
         return xmlDoc;
     };
 }
-//.title {
-//    box-shadow:  0px 0px 8px 6px  #cccccc; 
-//    color: #ffffff;
-//    text-shadow: 0px 0px 10px #000000;
-//.navigation {
-//    border: 2px solid #bbbbbb;
-//.list-item:hover {
-//    border: 2px solid #bbbbbb;
-//    box-shadow: 4px 4px 4px #bbbbbb;  /* "3D" effect on the main contianer list items */
+
+
+
+
+
+
+//D.title {
+//D    box-shadow:  0px 0px 8px 6px  #cccccc; 
+//D    color: #ffffff;
+//D    text-shadow: 0px 0px 10px #000000;
+//D.navigation {
+//D    border: 2px solid #bbbbbb;
+//?.list-item:hover {
+//?   border: 2px solid #bbbbbb;
+//?    box-shadow: 4px 4px 4px #bbbbbb;  /* "3D" effect on the main contianer list items */ 
 //.list-item-title {
 //    background-color: rgba(187, 187, 187, 1); /* lighter gray */
 //    color: #000000;

9c634e5e4b81530f3c429e913e5e974c5b27124d
James Skomsky <james.skomsky@gmail.com>
2020-01-20 02:36:29 -0500

    TODO: setup onclick to style body

--- Skomsky_HealthSystem/web/js/setTheme.js
+++ Skomsky_HealthSystem/web/js/setTheme.js
@@ -1,21 +1,40 @@
 /**
  * setTheme() will style/theme a set of properties toggling between two 
- * color palletes referred as light or dark. The color pallettes for each 
- * theme are customizable  via the settings XML. color.adobe.com  
- * provides a "copy to xml" function useful in creating the XML color palletes.
+ * palletes referred. The pallettes for each theme will eventually be  
+ * customizable via the settings XML. color.adobe.com  
+ * provides a "copy to xml" function useful in creating the XML color pallettes.
  * 
- * This fn. uses a cookie to persist the theme toggle value.  
+ * This fn. switches between themes styling various colors using a menu 
+ * bar icon as a toggle. When the icon is clicked the theme
+ * will switch back and forth. The theme settings are xml with two separate pallettes 
+ * each with providing the various colors.  
  * 
- * Various properties of the following HTML id's are themed in this function:
- *   "title"
- *   "nav"
- *   "list-item"
- *   "list-item-title"
- *   "mission"
- *   "mission-flyover"
- *   "footer"
+ * The available HTML id's and the list of each styled property are as follows;
+ *
+ *   id="title"
+ *       style.backgroundColor
+ *       style.boxShadow
+ *       style.color
+ *       style.textShadow
+ *   id="nav"
+ *       style.border
+ *       style.backgroundColor
+ *   id="list-item", "list-item-title"
+ *       style.backgroundColor
+ *       style.color
+ *   id="mission"
+ *       style.boxShadow
+ *       style.backgroundColor
+ *   id="mission-flyover"
+ *       style.color
+ *       style.textShadow
+ *   id="footer"
+ *       style.backgroundColor
+ *       style.boxShadow
+ *   id="theme-icon"
+ *      innerHTML=<img src=hash["icon"]
  * 
- * TODO: Dont have a way of making the XML a settings accessible to 
+ * TODO: Dont know of the best way to make the XML a settings accessible to 
  * the user to customize in an external xml file or anywhere other than 
  * hard-coded in the palletteDoc() fn. 
  * 
@@ -23,29 +42,19 @@
 
 "use strict";
 
-ddTheme.onclick = function () {
-    setTheme();
-}
+/**
+ * TODO: setup onclick fn. to restyle the document. 
+ * 
+ *ddTheme.onclick = function () {
+ *    setTheme();
+ *}
+*/
 
 function setTheme () {
 
-    var toggle = getToggle(); 
+    var palId = togglePallette();
+    var hash = getPallette(palId);
     
-    /* TODO: fix XOR */
-    /*var toggle = getToggle() ^ 1; /* XOR to switch back and forth from 0..1 */
-    if (toggle === "0") {
-        toggle = "1";
-    }
-    else
-    {
-        toggle = "0";
-    }
-    setCookie(toggle);
-    
-    var palId = new Array("LightPallette", "DarkPallette");
-
-    var hash = getPallette(palId[toggle]);
-
     /* theme the single elements */
     document.getElementById("title").style.backgroundColor = hash["titleBackgroundColor"];
     document.getElementById("title").style.boxShadow = "0px 0px 9px 7px " + hash["titleBoxShadow"];
@@ -59,15 +68,19 @@ function setTheme () {
     document.getElementById("mission-flyover").style.textShadow = "0px 0px 10px " + hash["missionFlyoverTextShadow"];
     document.getElementById("footer").style.backgroundColor = hash["footerBackgroundColor"];
     document.getElementById("footer").style.boxShadow = "0px 0px 9px 7px " + hash["footerBoxShadow"];
+    document.getElementById("footer").style.color = hash["footerColor"];
+    document.getElementById("footer").style.textShadow = "0px 0px 10px " + hash["footerTextShadow"];
 
     /* loop through and theme HTMLCollections elements */
     themeCollection(document.getElementsByClassName("list-item")); 
     themeCollection(document.getElementsByClassName("list-item-title")); 
 
+    /* style the icon */
+    document.getElementById("ddTheme").innerHTML = hash["icon"];
+
     /*
-     * getPallette() retuns a hash of the pallette/xml attributes with the color name
-     * as the hash value and the r,g,b,a attributes as a concatenated string "RGBA(r,g,b,a)"
-     * used to style the colors of a page. 
+     * getPallette() retuns a hash of the theme attributes. Color elements have 
+     * string "RGBA(r,g,b,a)" hash value, icon elements have a image src= hash value
      * 
      * @param {LightPallette|DarkPallette} palId
      * @returns {String} Hash
@@ -78,31 +91,40 @@ function setTheme () {
         var hash = {}; /* hash: key = descriptive color name */ 
                        /*       value = "RGBA(r,g,b,a)" string   */
         var xmlDoc = palletteDoc();
-        var pallette = xmlDoc.getElementById(palId); 
-        var child = pallette.firstChild;                       /* this is the first rgba color */
-        for (var i = 0; i < pallette.childNodes.length; i++) { /* for each get the rgba attrs */     
-            rgba = "rgba(";
-            rgba += child.getAttribute("r") + ", ";                
-            rgba += child.getAttribute("g") + ", ";     
-            rgba += child.getAttribute("b") + ", ";
-            alpha = child.getAttribute("a");
-            if (alpha === null) /* if not given alpha will default to l */
-            { 
-                rgba += "1)";
-            } 
-            else
-            {
-                rgba += alpha + ")";                
+        var theme = xmlDoc.getElementById(palId).childNodes; 
+        for (var i = 0; i < theme.length; i++) {
+            /* get the colors and concat the "RGBA(r,g,b,a)" string */
+            if (theme[i].id === "colors") {
+                var colors = theme[i].children;
+                for (var j = 0; j < colors.length; j++) { 
+                    rgba = "rgba(";
+                    rgba += colors[j].getAttribute("r") + ", ";                
+                    rgba += colors[j].getAttribute("g") + ", ";     
+                    rgba += colors[j].getAttribute("b") + ", ";
+                    alpha = colors[j].getAttribute("a");
+                    if (alpha === null) /* if not given alpha will default to l */
+                    { 
+                        rgba += "1)";
+                    } 
+                    else
+                    {
+                        rgba += alpha + ")";                
+                    }
+                    hash[colors[j].getAttribute("name")] = rgba; /* "name" will be hash key */
+                }
             }
-            hash[child.getAttribute("name")] = rgba; /* "name" will be hash key */
-            child = child.nextSibling;
-            rgba = "";           
+            /* add an icon to the theme */
+            if (theme[i].id === "icon") {
+                hash["icon"] = "<a><img class='theme-icon' id='theme-icon' title='theme' src='" +
+                                theme[i].getAttribute("src") + "'" +
+                                "</a>";
+            }
         }
         return hash;
     }
     /*
-     * palletteDoc() uses DOM XML to parse an input string of theme pallette/colors 
-     * into xml doc. 
+     * palletteDoc() implements DOM XML to parse an input string of theme pallettes/icons.
+     *
      * @returns {XML Document}
      */
     function palletteDoc() {
@@ -125,38 +147,48 @@ function setTheme () {
         var colorPalletteStr = 
             "<Root>" +
                 "<Pallette id='LightPallette'>" +
-                    "<color name='titleBackgroundColor'     rgb='F2F2DF' r='242' g='242' b='223' a='.5' />" +
-                    "<color name='titleBoxShadow'           rgb='CCCCCC' r='204' g='204' b='204' />" +
-                    "<color name='titleColor'               rgb='FFFFFF' r='355' g='255' b='255' />" +
-                    "<color name='titleTextShadow'          rgb='000000' r='000' g='000' b='000' />" +
-                    "<color name='navBorder'                rgb='B6F2F2' r='182' g='242' b='242' />" +
-                    "<color name='navBackground'            rgb='B6F2F2' r='182' g='242' b='242' a='.5' />" +
-                    "<color name='listItemBackground'       rgb='F2F2DF' r='242' g='242' b='223' />" +
-                    "<color name='listItemColor'            rgb='A7DDF2' r='167' g='221' b='242' />" +
-                    "<color name='listItemTitleColor'       rgb='A7E4F2' r='167' g='228' b='242' />" +
-                    "<color name='missionBackground'        rgb='B6F2F2' r='182' g='242' b='242' a='.5' />" +
-                    "<color name='missionBoxShadow'         rgb='4F82B3' r='137' g='197' b='255' />" +
-                    "<color name='missionFlyoverColor'      rgb='FFFFFF' r='255' g='255' b='255' />" +
-                    "<color name='missionFlyoverTextShadow' rgb='4F82B3' r='025' g='040' b='066' />" +
-                    "<color name='footerBackgroundColor'    rgb='F2F2DF' r='242' g='242' b='223' a='.5' />" +
-                    "<color name='footerBoxShadow'          rgb='CCCCCC' r='204' g='204' b='204' />" +
+                    "<colors id='colors'>" +
+                        "<color name='titleBackgroundColor'     rgb='F2F2DF' r='242' g='242' b='223' a='.5' />" +
+                        "<color name='titleBoxShadow'           rgb='CCCCCC' r='204' g='204' b='204' />" +
+                        "<color name='titleColor'               rgb='FFFFFF' r='355' g='255' b='255' />" +
+                        "<color name='titleTextShadow'          rgb='000000' r='000' g='000' b='000' />" +
+                        "<color name='navBorder'                rgb='B6F2F2' r='182' g='242' b='242' />" +
+                        "<color name='navBackground'            rgb='B6F2F2' r='182' g='242' b='242' a='.5' />" +
+                        "<color name='listItemBackground'       rgb='F2F2DF' r='242' g='242' b='223' />" +
+                        "<color name='listItemColor'            rgb='A7DDF2' r='167' g='221' b='242' />" +
+                        "<color name='listItemTitleColor'       rgb='A7E4F2' r='167' g='228' b='242' />" +
+                        "<color name='missionBackground'        rgb='B6F2F2' r='182' g='242' b='242' a='.5' />" +
+                        "<color name='missionBoxShadow'         rgb='4F82B3' r='137' g='197' b='255' />" +
+                        "<color name='missionFlyoverColor'      rgb='FFFFFF' r='255' g='255' b='255' />" +
+                        "<color name='missionFlyoverTextShadow' rgb='4F82B3' r='025' g='040' b='066' />" +
+                        "<color name='footerBackgroundColor'    rgb='F2F2DF' r='242' g='242' b='223' a='.5' />" +
+                        "<color name='footerBoxShadow'          rgb='CCCCCC' r='204' g='204' b='204' />" +
+                        "<color name='footerColor'              rgb='FFFFFF' r='355' g='255' b='255' />" +
+                        "<color name='footerTextShadow'         rgb='000000' r='000' g='000' b='000' />" +
+                    "</colors>" +
+                    "<icon id='icon' src='icons/light/sun-icon-H24.ico' />" + 
                 "</Pallette>" +
                 "<Pallette id='DarkPallette'>" +
-                    "<color name='titleBackgroundColor'     rgb='F2F2DF' r='242' g='242' b='223' />"+
-                    "<color name='titleBoxShadow'           rgb='4F82B3' r='079' g='130' b='179' />" +
-                    "<color name='titleColor'               rgb='000000' r='000' g='000' b='000' />" +
-                    "<color name='titleTextShadow'          rgb='4F82B3' r='029' g='080' b='129' />" +
-                    "<color name='navBorder'                rgb='A3D3FF' r='163' g='211' b='255' />" +
-                    "<color name='navBackground'            rgb='A3D3FF' r='163' g='211' b='255' a='.5' />" +
-                    "<color name='listItemBackground'       rgb='A3D3FF' r='163' g='211' b='255' />" +
-                    "<color name='listItemColor'            rgb='B3863D' r='179' g='134' b='61' />" +
-                    "<color name='listItemTitleColor'       rgb='B3863D' r='179' g='134' b='61' />" +
-                    "<color name='missionBackground'        rgb='A3D3FF' r='163' g='211' b='255' a='.5' />" +
-                    "<color name='missionBoxShadow'         rgb='4F82B3' r='100' g='150' b='200' />" +
-                    "<color name='missionFlyoverColor'      rgb='000000' r='000' g='000' b='000' />" +
-                    "<color name='missionFlyoverTextShadow' rgb='4F82B3' r='000' g='000' b='000' />" +
-                    "<color name='footerBackgroundColor'    rgb='F2F2DF' r='242' g='242' b='223' />"+
-                    "<color name='footerBoxShadow'          rgb='CCCCCC' r='204' g='204' b='204' />" +
+                    "<colors>" +
+                        "<color name='titleBackgroundColor'     rgb='F2F2DF' r='242' g='242' b='223' />"+
+                        "<color name='titleBoxShadow'           rgb='4F82B3' r='079' g='130' b='179' />" +
+                        "<color name='titleColor'               rgb='000000' r='000' g='000' b='000' />" +
+                        "<color name='titleTextShadow'          rgb='4F82B3' r='029' g='080' b='129' />" +
+                        "<color name='navBorder'                rgb='A3D3FF' r='163' g='211' b='255' />" +
+                        "<color name='navBackground'            rgb='A3D3FF' r='163' g='211' b='255' a='.5' />" +
+                        "<color name='listItemBackground'       rgb='A3D3FF' r='163' g='211' b='255' />" +
+                        "<color name='listItemColor'            rgb='B3863D' r='179' g='134' b='61' />" +
+                        "<color name='listItemTitleColor'       rgb='B3863D' r='179' g='134' b='61' />" +
+                        "<color name='missionBackground'        rgb='A3D3FF' r='163' g='211' b='255' a='.5' />" +
+                        "<color name='missionBoxShadow'         rgb='4F82B3' r='100' g='150' b='200' />" +
+                        "<color name='missionFlyoverColor'      rgb='000000' r='000' g='000' b='000' />" +
+                        "<color name='missionFlyoverTextShadow' rgb='4F82B3' r='000' g='000' b='000' />" +
+                        "<color name='footerBackgroundColor'    rgb='F2F2DF' r='242' g='242' b='223' />"+
+                        "<color name='footerBoxShadow'          rgb='CCCCCC' r='204' g='204' b='204' />" +
+                        "<color name='footerColor'              rgb='000000' r='000' g='000' b='000' />" +
+                        "<color name='footerTextShadow'         rgb='4F82B3' r='029' g='080' b='129' />" +
+                    "</colors>" +
+                "<icon id='icon' src='icons/light/moon-icon-H24.ico' />" + 
                 "</Pallette>" +
             "</Root>";
         var xmlDoc = document.implementation.createDocument("", "", null);
@@ -166,8 +198,7 @@ function setTheme () {
     }    
     /*
      * 
-     * @param {type} HTML collection
-     * @returns nothing returned. This is a "setter" type fn. 
+     * Theme multiple items of a HTML collection/CSS class. 
      */
     function themeCollection(collection) {
         var ele;
@@ -180,40 +211,19 @@ function setTheme () {
     
     /*
      * 
-     * @returns {Number|current state of theme (i.e. light/dark)}]
-     */
-    function getToggle() {
-        var toggle;
-        var cookie = document.cookie;  /* get the cookie string */
-        if (cookie === "") {           /* if not valued set an initial toggle state */
-            var cvalue = "1";
-            setCookie(cvalue);
-            toggle = 1;
-        }
-        else
-        {
-           var cookie = document.cookie;
-           var c = cookie.split(';');           /* cookies delimited by the ";" */
-           for (var i = 0; i < c.length; i++) {
-               var s = c[i].split('=');         /* each cookie has a "name=value" */
-               if (s[0] === "themeToggle") {    /* themeToggle cookie value is an array */
-                   toggle = s[1];               /* return the cookie toggle value */
-               }
-           }
-        }
-        return toggle;
-    }
-    
-    /*
-     * 
-     * @param {string} cValue | Name of cookie to be assigned.
-     * @returns {undefined}
-     */
-    function setCookie (cValue) {
-        var cName = "themeToggle"; /* cookie name */
-        var expDays = -1;
-        var d = new Date();        /* cookie expiration in milliseconds + getTime() */
-        d.setTime(d.getTime() + (expDays*24*60*60*1000));
-        document.cookie = cName + "=" + cValue + ";" + expDays + ";path=/";
+     * @returns {String} Togle between "LightPallette" or "DarkPallette" 
+     * depending on the current src= setting of the theme img icon. 
+     * Clicking on the dark theme icon switches to light theme and vice versa.
+     */    
+    function togglePallette() {
+        var currThemeIcon = document.getElementById("theme-icon").src;
+        var icon = currThemeIcon.match(/moon-icon/g); 
+        if (icon !== null) {
+            return "LightPallette";
+        };
+        var currThemeIcon = currThemeIcon.match(/sun-icon/g);  
+        if (icon !== null) {
+            return "DarkPallette";
+        };
     }
 }

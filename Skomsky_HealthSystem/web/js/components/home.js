505844149d08cacc13fe1401ef7d78600b876aca
James S <james.skomsky@gmail.com>
2020-07-07 17:42:28 -0400

    updated mission stmt and home page art

--- Skomsky_HealthSystem/web/js/components/home.js
+++ Skomsky_HealthSystem/web/js/components/home.js
@@ -7,7 +7,7 @@ function home(targetId) {
         <ul>
             <li>
                 <span class='list-item-title'>Services</span>
-                <img class='list-item-image' src='pics/med-svcs.jpg' alt=''/>
+                <img class='list-item-image' src='pics/med-svcs-color.jpg' alt=''/>
             </li>
             <li>
                 <span class='list-item-title'>Business Office</span>
@@ -22,11 +22,14 @@ function home(targetId) {
             <span class='mission-flyover' id='mission-flyover'>Our Mission</span>
             <p>
                 <img class='mission-icon' src='icons/dark/icon-mission-H300.png' />
-                Health Systems is a sample of my Front End Web Development work. 
-                It is a Single Page app using the Model-View-Controller design. 
-                It has create, read, update and delete (CRUD) functionality, and 
-                persists data in a MySQL database. It is built from the ground up (i.e. no BootStrap etc) 
-                using only foundation web technologies HTML, CSS, JavaScript, JSP, AJAX and Java. 
+                This web app is a sample of my Front End Web Development work. 
+                It is built from the ground up (i.e. no BootStrap etc) 
+                using only foundation web technologies HTML, CSS, JavaScript, 
+                JSP, AJAX and Java.
+                It is a Single Page app using the Model-View-Controller design.  
+                It has create, read, update and delete (CRUD) functionality. Explore
+                the drop down menu options and 'plus', 'pencil' and 'x' icons for each function. The 
+                data is persisted in a MySQL database.  
                 All security and authorization is enforced on the server side (in the Web APIs). 
                 The (logged on) user is verified by checking the JSP Implicit Session Object. 
                 The JSPs retrieve URL parameters using a getParameter on the Request Session Object. 
